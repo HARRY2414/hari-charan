ae279530a8474688ad85aca8e84f1add7c4c5a55
Hari Charan Indrakanti <111627538+HARRY2414@users.noreply.github.com>
2023-08-20 18:17:35 -0500

    Add files via upload

---
+++ AndroidNotes/new_notes.java
@@ -0,0 +1,16 @@
+package com.christopherhield.AndroidNotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidnotes.R;
+
+public class new_notes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_new_notes );
+    }
+}
